46e9e2e0f658355a684881de677e8cd538ce3e97
AlvaroBrizRiv <a.brizuela.r@gmail.com>
2026-04-08 20:46:20 -0300

    Reloj y formulario para registrar articulo

--- script.js
+++ script.js
@@ -5,5 +5,32 @@ function actualizarReloj()
     contenedorReloj.innerText = "Hora en Chile: " + fecha.toLocaleTimeString();
 
 }
-actualizarReloj();
-setInterval(actualizarReloj, 1000);
+
+const formulario = document.getElementById("formulario-articulo");
+
+formulario.addEventListener("submit", function(evento){
+
+    evento.preventDefault();
+    
+    const imagenURL = document.getElementById('imagen-articulo').value;
+    const titulo = document.getElementById('titulo-articulo').value;
+    const descripcion = document.getElementById('descripcion-articulo').value;
+    const categoria = document.getElementById('categoria-articulo').value;
+    const link = document.getElementById('link-articulo').value;
+    const nuevoArticulo = document.createElement('article');
+
+    nuevoArticulo.innerHTML = `
+    <img src="${imagenURL}" width="750" height="265" alt="Imagen del artículo"/>
+        <div class="info-articulo">
+            <p><strong>${titulo}</strong></p>
+            <p>${descripcion}</p>
+            <p class="${categoria}">Más información haciendo click <a href="${link}" target="_blank">aquí</a></p>
+        </div>
+    `;
+
+    const seccionDestino = document.getElementById('seccion-' + categoria);
+    seccionDestino.prepend(nuevoArticulo);
+
+    formulario.reset();
+
+})
